4335be145c007a629d8a8403e42984d026978c61
Akhil <Akhil@LAPTOP-3O8A24HK>
2020-02-26 23:14:53 +0530

    logging

---
+++ logging/src/main/java/logging/MaterialsRequiredToConstructHouse.java
@@ -0,0 +1,20 @@
+package logging;
+
+public class MaterialsRequiredToConstructHouse {
+	String materialType;
+	   double totalAreaOfHouse;
+	   String automationType;
+	   MaterialsRequiredToConstructHouse(){}
+	   MaterialsRequiredToConstructHouse(String materialType,double totalAreaOfHouse,String automationType)
+	   {
+		   this.materialType=materialType;
+		   this.totalAreaOfHouse=totalAreaOfHouse;
+		   this.automationType=automationType;
+	   }
+	   public String calculateHouseConstructionCost(String materialType, double totalAreaOfHouse, String automationType) throws Exception
+	   {
+		   HouseConstructionCost houseConstructionCost=new  HouseConstructionCost(materialType, totalAreaOfHouse, automationType);
+		   return String.valueOf(houseConstructionCost.computeCostOfHouseConstruction())+"INR";
+	   }
+
+}
